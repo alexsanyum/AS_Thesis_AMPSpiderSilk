38d5d42365cac8af255b5e0b2890d18f9b6f8478
alexsanyum <66842564+alexsanyum@users.noreply.github.com>
2020-10-23 11:27:06 -0500

    Add files via upload

--- clusters_files/cluster_data.sh
+++ clusters_files/cluster_data.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 
-~/thesis/MeShClust/bin/meshclust ../EST_data/Latrodectushesperus.fasta --id 0.60 --kmer 5 --delta 5 --output Latrodectushesperus.fasta.clstr --iterations 20 --align --sample 3000 --pivot 40 --threads 1
-~/thesis/MeShClust/bin/meshclust ../EST_data/Leucaugevenusta_2.fasta --id 0.60 --kmer 5 --delta 5 --output Leucaugevenusta_2.fasta.clstr --iterations 20 --align --sample 3000 --pivot 40 --threads 1
-~/thesis/MeShClust/bin/meshclust ../EST_data/Leucaugevenusta.fasta --id 0.60 --kmer 5 --delta 5 --output Leucaugevenusta.fasta.clstr --iterations 20 --align --sample 3000 --pivot 40 --threads 1
-~/thesis/MeShClust/bin/meshclust ../EST_data/Nephilaantipodiana.fasta --id 0.60 --kmer 5 --delta 5 --output Nephilaantipodiana.fasta.clstr --iterations 20 --align --sample 3000 --pivot 40 --threads 1
-~/thesis/MeShClust/bin/meshclust ../EST_data/Parasteatodatepidariorum.fasta --id 0.60 --kmer 5 --delta 5 --output Parasteatodatepidariorum.fasta.clstr --iterations 20 --align --sample 3000 --pivot 40 --threads 1
-~/thesis/MeShClust/bin/meshclust ../EST_data/Steatodagrossa.fasta --id 0.60 --kmer 5 --delta 5 --output Steatodagrossa.fasta.clstr --iterations 20 --align --sample 3000 --pivot 40 --threads 1
+echo ${1}
+~/thesis/MeShClust/bin/meshclust ../EST_data/Latrodectushesperus.fasta --id 0.${1} --kmer 5 --delta 5 --output Latrodectushesperus_clusters/Latrodectushesperus_0_${1}.clstr --iterations 20 --align --sample 3000 --pivot 40 --threads 1
+~/thesis/MeShClust/bin/meshclust ../EST_data/Leucaugevenusta_2.fasta --id 0.${1} --kmer 5 --delta 5 --output Leucaugevenusta_2_clusters/Leucaugevenusta_2_0_${1}.clstr --iterations 20 --align --sample 3000 --pivot 40 --threads 1
+~/thesis/MeShClust/bin/meshclust ../EST_data/Leucaugevenusta.fasta --id 0.${1} --kmer 5 --delta 5 --output Leucaugevenusta_clusters/Leucaugevenusta_0_${1}.clstr --iterations 20 --align --sample 3000 --pivot 40 --threads 1
+~/thesis/MeShClust/bin/meshclust ../EST_data/Nephilaantipodiana.fasta --id 0.${1} --kmer 5 --delta 5 --output Nephilaantipodiana_clusters/Nephilaantipodiana_0_${1}.clstr --iterations 20 --align --sample 3000 --pivot 40 --threads 1
+~/thesis/MeShClust/bin/meshclust ../EST_data/Parasteatodatepidariorum.fasta --id 0.${1} --kmer 5 --delta 5 --output Parasteatodatepidariorum_clusters/Parasteatodatepidariorum_0_${1}.clstr --iterations 20 --align --sample 3000 --pivot 40 --threads 1
+~/thesis/MeShClust/bin/meshclust ../EST_data/Steatodagrossa.fasta --id 0.${1} --kmer 5 --delta 5 --output Steatodagrossa_clusters/Steatodagrossa_0_${1}.clstr --iterations 20 --align --sample 3000 --pivot 40 --threads 1
+
+
---
+++ clusters_files/run_all_clusters.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+./cluster_data.sh 60
+./cluster_data.sh 65
+./cluster_data.sh 70
+./cluster_data.sh 75
+./cluster_data.sh 80
+./cluster_data.sh 85
+./cluster_data.sh 90
